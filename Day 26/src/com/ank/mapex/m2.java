ef790a032891ef87ed4ff9d2760d9371fc1b8fee
AnK <AnK@DESKTOP-35CEPU1>
2022-08-12 03:44:17 +0530

    day 26 code

---
+++ Day 26/src/com/ank/mapex/m2.java
@@ -0,0 +1,14 @@
+package com.ank.mapex;
+import java.util.*;  
+class m2{  
+ public static void main(String args[]){  
+Map<Integer,String> map=new HashMap<Integer,String>();          
+      map.put(100,"Amit");    
+      map.put(101,"Vijay");    
+      map.put(102,"Rahul");       
+      map.entrySet()  
+      .stream()  
+      .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))   
+      .forEach(System.out::println);  
+ }  
+}  
---
+++ Day 26/src/com/ank/mapex/m3.java
@@ -0,0 +1,14 @@
+package com.ank.mapex;
+import java.util.*;  
+class m3{  
+ public static void main(String args[]){  
+Map<Integer,String> map=new HashMap<Integer,String>();          
+      map.put(100,"Amit");    
+      map.put(101,"Vijay");    
+      map.put(102,"Rahul");       
+      map.entrySet()   
+      .stream()  
+      .sorted(Map.Entry.comparingByValue())  
+      .forEach(System.out::println);  
+ }  
+}  
